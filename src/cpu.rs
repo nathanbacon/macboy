8998a87847532f21aa4db53087c586b700c37b13
Nathan Gelman <nathanjlg@gmail.com>
2023-01-16 15:08:57 -0800

    added timing for rr b

--- src/cpu.rs
+++ src/cpu.rs
@@ -3804,6 +3804,7 @@ use super::*;
     cpu.call(0xCB);
     cpu.call(0x18);
 
+    assert_eq!(cpu.ticks, 8);
     assert_eq!(cpu.registers.b, 0b11000001, "{:#010b} != {:#010b}", cpu.registers.b, 0b11000001);
     assert!(cpu.registers.get_carry());
     assert!(!cpu.registers.get_half_carry());
