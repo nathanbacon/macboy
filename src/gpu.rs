bcfda92da04e531d991fd01072b4a6f0494caf33
Nathan Gelman <nathanjlg@gmail.com>
2023-02-14 17:02:03 -0800

    completed memory map

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,13 +1,26 @@
 pub struct GPU {
   memory: Box<[u8; 0x2000]>,
+  oam: Box<[u8; 0xA0]>
 }
 
 impl GPU {
   pub fn new() -> GPU {
     GPU {
       memory: Box::new([0u8; 0x2000]),
+      oam: Box::new([0u8; 0xA0]),
     }
   }
+
+  pub fn read(&self, address: usize) -> u8 {
+    let address = address & 0x1FFF;
+    self.memory[address]
+  }
+
+  pub fn write(&mut self, address: usize, value: u8) {
+    let address = address & 0x1FFF;
+    self.memory[address] = value;
+  }
+
   pub fn subscript(&mut self, index: usize) -> &mut u8 {
     &mut self.memory[index]
   }
--- src/mmu.rs
+++ src/mmu.rs
@@ -6,6 +6,7 @@ use crate::cartridge::{Catridge, MBC, MBC3};
 pub struct MMU<T> where T: MBC {
   gpu: GPU,
   mbc: T,
+  working_memory: Box<[u8; 0x2000]>,
 }
 
 impl<T: MBC> MMU<T> {
@@ -13,6 +14,7 @@ impl<T: MBC> MMU<T> {
     MMU {
       gpu: GPU::new(),
       mbc,
+      working_memory: Box::new([0u8; 0x2000]),
     }
   }
   
@@ -26,7 +28,32 @@ impl<T: MBC> MMU<T> {
     let address = address as usize;
     match address {
       0x0000..=0x7FFF => self.mbc.read(address),
+      0x8000..=0x9FFF => self.gpu.read(address),
       0xA000..=0xBFFF => self.mbc.read(address),
+      0xC000..=0xDFFF => {
+        let address = 0x1FFF & address;
+        self.working_memory[address]
+      },
+      0xE000..=0xFDFF => {
+        // forbidden according to manual but in actuality, it's a echo of working ram
+        panic!("unimplemented read to 0xE000..=0xFDFF")
+      },
+      0xFE00..=0xFE9F => {
+        // TODO: implement OAM access here
+        panic!("unimplemented OAM read");
+      },
+      0xFEA0..=0xFEFF => {
+        panic!("unimplemented");
+      },
+      0xFF00..=0xFF7F => {
+        panic!("unimplemented");
+      },
+      0xFF80..=0xFFFE => {
+        panic!("unimplemented");
+      },
+      0xFFFF => {
+        panic!("unimplemented");
+      },
       _ => panic!("unimplemented address space"),
     }
   }
@@ -35,7 +62,32 @@ impl<T: MBC> MMU<T> {
     let address = address as usize;
     match address {
       0x0000..=0x7FFF => self.mbc.write(address, value),
+      0x8000..=0x9FFF => self.gpu.write(address, value),
       0xA000..=0xBFFF => self.mbc.write(address, value),
+      0xC000..=0xDFFF => {
+        let address = address & 0x1FFF;
+        self.working_memory[address] = value;
+      },
+      0xE000..=0xFDFF => {
+        // forbidden according to manual but in actuality, it's a echo of working ram
+        panic!("unimplemented write to 0xE000..=0xFDFF")
+      },
+      0xFE00..=0xFE9F => {
+        // TODO: implement OAM access here
+        panic!("unimplemented OAM write");
+      },
+      0xFEA0..=0xFEFF => {
+        panic!("unimplemented");
+      },
+      0xFF00..=0xFF7F => {
+        panic!("unimplemented");
+      },
+      0xFF80..=0xFFFE => {
+        panic!("unimplemented");
+      },
+      0xFFFF => {
+        panic!("unimplemented");
+      },
       _ => panic!("unimplemented address space!"),
     }
   }
